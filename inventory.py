9208dd400d06d92b16fbff7afed461ec3e3cb4b6
cfurlotte <cfurlotte@ivytech.edu>
2023-11-29 16:43:13 -0500

    Updated with a list of ingredients

--- inventory.py
+++ inventory.py
@@ -3,6 +3,15 @@ class Inventory:
         #Charles code to show way program works
         #max amount of a product
         self.maxDough = 500 #oz
+        self.maxTomatoSauce = 300 #oz
+        self.maxCheese = 300 #oz
+        self.maxOliveOil = 100 #oz
+        self.maxPepperoni = 100 #oz
+        self.maxSausage = 100 #oz
+        self.maxHam = 100 #oz
+        self.maxMushrooms = 75 #oz
+        self.maxOnions = 75 #oz
+        self.maxPeppers = 75 #oz
         #current amount, should be stored in a file, or create a file if file does not exist
         self.currentDough = self.maxDough
 
