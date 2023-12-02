5089836b431b845a24a6d4f510632c55789ea06a
DavidMBowman <125861918+DavidMBowman@users.noreply.github.com>
2023-12-02 00:35:28 -0500

    Update inventory.py
    I created a working database, there's load_inventory, save_inventory, change_inventory, and reset_inventory functions, I'll explain what they do and how to use them in the discord. The database file just needs to be run once to initialize it.

--- inventory.py
+++ inventory.py
@@ -1,22 +1,41 @@
-class Inventory:
-    def __init__(self):
-        #Charles code to show way program works
-        #max amount of a product
-        self.maxDough = 500 #oz
-        self.maxTomatoSauce = 300 #oz
-        self.maxCheese = 300 #oz
-        self.maxOliveOil = 100 #oz
-        self.maxPepperoni = 100 #oz
-        self.maxSausage = 100 #oz
-        self.maxHam = 100 #oz
-        self.maxMushrooms = 75 #oz
-        self.maxOnions = 75 #oz
-        self.maxPeppers = 75 #oz
-        #current amount, should be stored in a file, or create a file if file does not exist
-        self.currentDough = self.maxDough
-        #continue from the above
-        
-    #example layout of a function I can call to change amount of something.
-    def changeDoughAmount(self, amountToChange):
-        self.currentDough -= amountToChange
-        print(self.currentDough)
+import pickle
+
+def load_inventory():
+    try:
+        with open('inventory.pkl', 'rb') as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return {}
+
+def save_inventory(inventory):
+    with open('inventory.pkl', 'wb') as f:
+        pickle.dump(inventory, f)
+
+def change_inventory(inventory, item, amount):
+    if item in inventory:
+        inventory[item]['current'] += amount
+    else:
+        print(f"Item {item} not found in inventory.")
+    save_inventory(inventory)
+
+def reset_inventory(inventory, item):
+    if item in inventory:
+        inventory[item]['current'] = inventory[item]['max']
+    else:
+        print(f"Item {item} not found in inventory.")
+    save_inventory(inventory)
+
+if __name__ == "__main__":
+    inventory = {
+        'Dough': {'current': 25, 'max': 500},
+        'TomatoSauce': {'current': 25, 'max': 300},
+        'Cheese': {'current': 25, 'max': 300},
+        'OliveOil': {'current': 25, 'max': 100},
+        'Pepperoni': {'current': 25, 'max': 100},
+        'Sausage': {'current': 25, 'max': 100},
+        'Ham': {'current': 25, 'max': 100},
+        'Mushrooms': {'current': 25, 'max': 75},
+        'Onions': {'current': 25, 'max': 75},
+        'Peppers': {'current': 25, 'max': 75},
+    }
+    save_inventory(inventory)
